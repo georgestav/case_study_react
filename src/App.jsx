fe649e8845dbbe4943243cd4a7e0ac5fc6260f42
George Stavroulakis <g.stavroulakis@gmail.com>
2022-03-30 11:26:36 +0200

    result

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.scss";
+import Comparison from "./components/Comparison";
+import Estates from "./components/Estates";
+
+function App() {
+	const [estates, setEstates] = useState([]);
+	const [compareEstates, setCompareEstates] = useState([]);
+	const fetchEstates = async () => {
+		try {
+			const request = await axios.get(
+				"https://estate-comparison.codeboot.cz/list.php?limit=9"
+			);
+			const response = await request.data;
+			setEstates(response);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	useEffect(() => {
+		fetchEstates();
+	}, []);
+
+	return (
+		<div className="comparison__container">
+			{/* h2 assuming here that the component is to be included in a page with a defined h1 */}
+			<h2 className="comparison__title">Estate Comparison</h2>
+			<div className="estates">
+				<Estates
+					estates={estates}
+					compareEstates={compareEstates}
+					setCompareEstates={setCompareEstates}
+				/>
+			</div>
+			<div className="compare__estates__container">
+				<Comparison compareEstates={compareEstates} />
+			</div>
+		</div>
+	);
+}
+
+export default App;
